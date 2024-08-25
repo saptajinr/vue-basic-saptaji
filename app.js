fac9e3ca8db22f664b77200a7c45aa58ff20f732
Saptaji Kurniawan <saptaji.kurniawan@gmail.com>
2024-08-25 20:10:59 +0700

    create app.js

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
+
 var app = new Vue({
     el: '#app',
     data: {
-        maximum: 50,
-        products: null,
-        cart: []
-    },
-    mounted: function() {
-        fetch('https://hplussport.com/api/products/order/price')
-        .then(response => response.json())
-        .then(data => {
-            this.products = data;
-        });
-    },
-    methods: {
-        addItem: function (product) {
-            this.cart.push(product);
-        }
+        name: 'Vue.js'
     }
 });
