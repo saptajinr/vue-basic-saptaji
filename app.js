861ec317c40ca321cf56899da1d0c0457b979cca
Saptaji Kurniawan <saptaji.kurniawan@gmail.com>
2024-08-25 22:27:04 +0700

    Fetch data di vue

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-
 var app = new Vue({
     el: '#app',
     data: {
-        name: 'Vue.js'
+        maximum: 50,
+        products: null
+    },
+    mounted: function () {
+        fetch('https://hplussport.com/api/products/order/price')
+            .then(response => response.json())
+            .then(data => {
+                this.products = data;
+            });
     }
 });
